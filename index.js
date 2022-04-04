1fd6cce580c823ad62143bcbd8327222824547d2
moinsoft <mmoin.official@gmail.com>
2022-04-04 23:07:42 +0600

    Added Procfile For Deployment In Heroku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(require('./routes'))
