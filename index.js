82612dabd870fb85f65f2c87e598763ee8624001
moinsoft <mmoin.official@gmail.com>
2022-04-04 17:53:58 +0600

    Create Form

--- index.js
+++ index.js
@@ -3,11 +3,21 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
+app.use(express.urlencoded({ extended: true }));
+
 app.set('view engine', 'ejs');
 
+let pLanguages = [];
+
 app.get('/', (req, res) => {
-  res.render('home', {});
+  res.render('home', {plNames: pLanguages});
 })
+
+app.post('/', (req, res) => {
+  const pLanguage = req.body.pLanguage;
+  pLanguages.push(pLanguage);
+  res.redirect('/')
+});
 
 app.listen(port, () => {
   console.log(`Application is ready to server on PORT ${port}`);
